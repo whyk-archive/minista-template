4e0aff00b4993cd047be72d591fcaf61a1ef464d
WhyK <windchimeyk@gmail.com>
2021-10-03 01:28:01 +0900

    feat: change build tool
    ts and babel to esbuild

--- webpack.config.js
+++ webpack.config.js
@@ -13,18 +13,65 @@ const htmlPluginList = glob.sync('**/*.tsx', {cwd: 'src/pages'}).map((file) => {
   })
 })
 
+const IS_DEV = process.env.NODE_ENV === 'development';
+
 const webpackConfig = {
   entry: './src/assets/index.ts',
   module: {
     rules: [
       {
-        test: [/\.ts$/, /\.tsx$/, /\.js$/],
+        test: /\.ts$/,
         exclude: /node_modules/,
-        use: ['babel-loader', 'ts-loader'],
+        use: [
+          {
+            loader: 'thread-loader',
+            options: {
+              workers: 2,
+              workerParallelJobs: 80,
+              workerNodeArgs: ['--max-old-space-size=512'],
+              name: 'ts-loader-pool',
+            }
+          },
+          {
+            loader: 'esbuild-loader',
+            options: {
+              loader: 'ts',
+              minify: !IS_DEV,
+              target: 'es2015',
+            },
+          }
+        ],
+      },
+      {
+        test: /\.tsx$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'thread-loader',
+            options: {
+              workers: 2,
+              workerParallelJobs: 80,
+              workerNodeArgs: ['--max-old-space-size=512'],
+              name: 'tsx-loader-pool',
+            }
+          },
+          {
+            loader: 'esbuild-loader',
+            options: {
+              // loaderはstring型なのでtsとtsxファイルの設定を別々に定義する必要がある
+              loader: 'tsx',
+              minify: !IS_DEV,
+              target: 'es2015',
+            },
+          }
+        ],
       },
     ],
   },
   resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
   plugins: [...htmlPluginList],
